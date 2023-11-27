b63dc5e6576e92732f30273e8deb3a4fe1d7b74f
Ivan <123373115+Gr8ivan@users.noreply.github.com>
2023-11-27 15:11:32 +0000

    Added database chema, user js, login and sing up

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+const bcrypt = require('bcrypt');
+const User = require('./models/user');
 
 const app = express();
 const port = process.env.PORT || 3000;
 const db = mongoose.connection;
+
 
 // Middleware
 app.use(cors());
@@ -36,4 +39,36 @@ app.get('/', (req, res) => {
 // Start server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+
+// Login Endpoint
+app.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (user && await bcrypt.compare(password, user.password)) {
+      // User found and password is correct
+      res.send('Login successful');
+    } else {
+      // User not found or password does not match
+      res.status(401).send('Invalid credentials');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred');
+  }
+});
+
+// Sign Up
+app.post('/signup', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10); // Hash the password
+    const user = new User({ username, password: hashedPassword });
+    await user.save(); // Save the new user
+    res.status(201).send('User successfully created.');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error creating user.');
+  }
 });
--- user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  // Add other fields as needed
-});
-
-module.exports = mongoose.model('User', userSchema);
